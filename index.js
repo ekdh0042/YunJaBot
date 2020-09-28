50da13e1080e05eb9c1f74d30a309a889b72f415
ekdh0042 <suwongood1234@gmail.com>
2020-09-28 11:52:19 +0900

    add serverinfo

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const client = new Discord.Client();
 const token = process.argv.length == 2 ? process.env.token : "";
 const moment = require("moment");
 require("moment-duration-format");
-const welcomeChannelName = "출입";
-const byeChannelName = "퇴장";
-const welcomeChannelComment = "반가워";
-const byeChannelComment = "잘가.";
+const welcomeChannelName = "안녕하세요";
+const byeChannelName = "안녕히가세요";
+const welcomeChannelComment = "어서오세요.";
+const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
@@ -20,7 +20,7 @@ client.on("guildMemberAdd", (member) => {
 
   welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
 
-  member.addRole(guild.roles.find(role => role.name == "유저"));
+  member.addRole(guild.roles.find(role => role.name == "게스트"));
 });
 
 client.on("guildMemberRemove", (member) => {
@@ -38,23 +38,11 @@ client.on('message', (message) => {
     return message.reply('pong');
   }
 
-  if(message.content == 'embed') {
-    let img = 'https://cdn.discorhttps://cdn.discordapp.com/attachments/672689142152429578/759966236346941531/814d64c313ef9fbf93f5f6a1a86706c09ada6bc8b3fc80787102eaf87108996d.jpgdapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
+  if(message.content == '!si') {
     let embed = new Discord.RichEmbed()
-      .setTitle('타이틀')
-      .setURL('http://www.naver.com')
-      .setAuthor('뽄뉴니', img, '')
-      .setThumbnail(img)
-      .addBlankField()
-      .addField('Inline field title', '으악')
-      .addField('Inline field title', '클래스룸 싫다', true)
-      .addField('Inline field title', '귀찮다', true)
-      .addField('Inline field title', '배고프다', true)
-      .addField('Inline field title', '귀찮음\힘듦\배고픔\n')
-      .addBlankField()
-      .setTimestamp()
-      .setFooter('뽄뉴니가 만듬', img)
-
+    let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
+    var duration = moment.duration(client.uptime).format(" D [일], H [시간], m [분], s [초]");
+    
     message.channel.send(embed)
   } else if(message.content == '!help') {
     let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
@@ -68,7 +56,7 @@ client.on('message', (message) => {
       {name: '!초대코드', desc: '해당 채널의 초대 코드 표기'},
       {name: '!초대코드2', desc: '봇이 들어가있는 모든 채널의 초대 코드 표기'},
     ];
-    
+
     commandList.forEach(x => {
       commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
     });
@@ -78,7 +66,7 @@ client.on('message', (message) => {
     message.channel.send(embed)
   } else if(message.content == '!초대코드2') {
     client.guilds.array().forEach(x => {
-      x.channels.find(x => x.type == 'text').createInvite // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
+      x.channels.find(x => x.type == 'text').createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
         .then(invite => {
           message.channel.send(invite.url)
         })
@@ -104,7 +92,6 @@ client.on('message', (message) => {
   } else if(message.content.startsWith('!전체공지2')) {
     if(checkPermission(message)) return
     if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!전체공지2'.length);
   
       embed.addField('공지: ', contents);
   
